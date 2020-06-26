d0841085a0b07c016ea0cf9e90ccc028a86ed426
Peter Boone <boonepeterg@gmail.com>
2020-06-26 13:16:21 -0400

    allow cross site origins

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, make_response
 from recipe_scrapers import scrape_me
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/api/parse', methods=["GET"])
 def parse_recipe():
@@ -54,7 +57,6 @@ def about_api():
                 "image": ""
             }
         </code>
-        
 
     """
 
